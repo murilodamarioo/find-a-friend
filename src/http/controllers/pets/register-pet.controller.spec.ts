98a4b2b67b479fe2e2bfe4d8288bf87afa3329bf
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-10 16:07:14 -0300

    feat: implements E2E test -  register pet

---
+++ src/http/controllers/pets/register-pet.controller.spec.ts
@@ -0,0 +1,38 @@
+import { app } from '@/app'
+import { makeOrg } from '@/tests/factories/make-org.factory'
+import { makePet } from '@/tests/factories/make-pet.factory'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+
+
+describe('Register Pet controller test', () => {
+  
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a pet', async () => {
+    const org = makeOrg()
+
+    await request(app.server).post('/orgs').send(org)
+
+    const authResponse = await request(app.server).post('/auth').send({
+        email: org.email,
+        password: org.password
+    })
+
+    const { token } = authResponse.body
+    
+    const response = await request(app.server)
+    .post('/pets')
+    .set('Authorization', `Bearer ${token}`)
+    .send(makePet())
+    
+    expect(response.statusCode).toBe(201)
+  })
+
+})

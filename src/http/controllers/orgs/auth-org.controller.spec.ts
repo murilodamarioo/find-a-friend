856873eb083be859638cf013dee6c50aafbf2d92
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-10 15:49:09 -0300

    feat: implements E2E test - Auth Org

---
+++ src/http/controllers/orgs/auth-org.controller.spec.ts
@@ -0,0 +1,32 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { makeOrg } from '@/tests/factories/make-org.factory'
+
+
+describe('Auth Org controller tests', () => {
+  
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('shoul be able to auth an org', async () => {
+    const org = makeOrg()
+
+    await request(app.server).post('/orgs').send(org)
+    
+    const response = await request(app.server)
+      .post('/auth')
+      .send({
+        email: org.email,
+        password: org.password
+      })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.token).toEqual(expect.any(String))
+  })
+})
